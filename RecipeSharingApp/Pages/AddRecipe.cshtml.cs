a9c101d89f8f0dd0cddef5997080a37470ac86f0
ioanamv <ioana.vadineanu@yahoo.com>
2025-01-25 11:43:05 +0200

    updated add recipe/comment

--- RecipeSharingApp/Pages/AddRecipe.cshtml.cs
+++ RecipeSharingApp/Pages/AddRecipe.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RecipeSharingApp.Data;
 using RecipeSharingApp.Models;
+using System.Security.Claims;
 
 namespace RecipeSharingApp.Pages
 {
@@ -14,27 +15,41 @@ namespace RecipeSharingApp.Pages
 
         public List<string> AvailableCategories { get; set; } = new List<string> { "Breakfast", "Main Dish", "Dessert" };
         public List<string> AvailableRegions { get; set; } = new List<string> { "Romania", "Italy", "UK", "China", "Japan", "Nigeria", "South Africa" };
-        
+
+        private readonly AppDbContext _context;
+
+        public AddRecipeModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
             Recipe=new Recipe();
         }
 
-        //public IActionResult OnPost(IFormFile image)
-        //{
-            //if (image != null && image.Length > 0)
-            //{
-            //    var filePath = Path.Combine("wwwroot/images", image.FileName);
-            //    using (var stream = new FileStream(filePath, FileMode.Create))
-            //    {
-            //        image.CopyTo(stream);
-            //    }
-            //    Recipe.ImageUrl = $"/images/{image.FileName}";
-            //}
+        public IActionResult OnPost(IFormFile image)
+        {
+            if (image != null && image.Length > 0)
+            {
+                var filePath = Path.Combine("wwwroot/images", image.FileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    image.CopyTo(stream);
+                }
+                Recipe.ImageUrl = $"/images/{image.FileName}";
+            }
 
-            //RecipesData.AddRecipe(Recipe);
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+            if (userId == null)
+                Forbid();
 
-            //return RedirectToPage("/Index");
-        //}
+            Recipe.UserId=int.Parse(userId.Value);
+            
+            _context.Recipes.Add(Recipe);
+            _context.SaveChanges();
+
+            return RedirectToPage("/Index");
+        }
     }
 }
